b95a8d19c24f0b454660d99eb8071de4e70f4328
BadApple <1076351865@qq.com>
2016-10-21 00:25:14 +0800

    增加广告模块
    增加广告模块

--- mvp_php/ad/splash.php
+++ mvp_php/ad/splash.php
@@ -1,5 +1,12 @@
 <?php
-    $rs['name'] = '0';
-    $rs['img'] = "http://imgsrc.baidu.com/forum/pic/item/af1ba5246b600c33bc2213191a4c510fd8f9a105.jpg";
+    require("db_config.php");
+    $conn=mysql_connect($mysql_server_name,$mysql_username,$mysql_password) or die("error connecting") ;
+    mysql_query("set names 'utf8'");
+    mysql_select_db($mysql_database);
+    $sql="select url,name from ad";
+    $rowz=mysql_fetch_row(mysql_query($sql));
+    $rs['name'] = $rowz[1];
+    $rs['img'] = $rowz[0];
+    //$rs['img'] = "http://img.blog.csdn.net/20161021000000845";
     echo json_encode($rs);
 ?>
